7d5899563d7d85ee16696f26af2aad0546936694
Willyb15 <william.steen.bryant@gmail.com>
2016-04-27 09:54:16 -0400

    Updated Controller

--- front-end/controller.js
+++ front-end/controller.js
@@ -3,8 +3,6 @@ var searchApp = angular.module('searchApp', []);
 searchApp.controller('searchController', function($scope, $http){
 	
 	$scope.search = function(){
-		$scope.message('Hello World');
+		$scope.message = 'Hello World';
 	};
-
-
 });
